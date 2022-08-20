4541edcd9cbca1b4c5c9535a3031188bdb712334
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-08-20 21:20:00 +0200

    I started creating the logic
    behind my countdown by selecting the current time and the time remaining to my deadline

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,29 @@
 /* Declaration of my strict mode */
-'use strict'
+/* 'use strict' */
 
+const countdown = document.querySelector('.countdown')
+console.log(countdown);
+
+/* This is my Deadline */
+const deadLine = new Date(2022, 7, 22, 12, 00, 00)
+console.log(deadLine);
+
+/* This is my Current time (expressed in milliseconds) */
+const currentTime = new Date()
+console.log(currentTime);
+
+/* This is the difference between de Deadline and the currenttime */
+const difference = deadLine - currentTime;
+console.log(difference);
+
+/* These are my countdown elements */
+const days = Math.floor(difference / (100 * 60 * 60 * 20)) /* These are my Days */
+const hours = Math.floor(difference / (1000 * 60 * 60) % 24) /* These are my Hours */
+const minutes = Math.floor(difference / (1000 * 60) % 60) /* These are my Minutes */
+const seconds = Math.floor((difference / 1000) % 60) /* These are my Seconds */
+
+/* These are my console Logs  */
+console.log(`These are the remain seconds before the deadline : ${days}`);
+console.log(`These are the remain hours before the deadline : ${hours}`);
+console.log(`These are the remain minutes before the deadline : ${minutes}`);
+console.log(`These are the remain seconds before the deadline : ${seconds}`);
